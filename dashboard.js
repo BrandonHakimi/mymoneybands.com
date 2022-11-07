96b5594b1340e90793934dab58b4833ff2197735
Chris-Araklisianos <christopher.araklisianos.733@my.csun.edu>
2022-11-06 22:44:34 -0800

    Added AddBill method in dashboard.js and onclick action for the function in dashboard.html
    Added an onclick action in dashboard.html to add functionality of AddBill() method in dashboard.js. This function allow for multiple individual bills to be stored in the text file for the user. I have added an initial implementation of the AddBill method within dashboard.js.

--- dashboard.js
+++ dashboard.js
@@ -1,2 +1,19 @@
 
+function AddBill() {
+    do {
+        var Continue = 1;
+        var BillName = prompt("Who is this Bill due to? (Company or Label)");
+        var BillValue = prompt("What is the Payment Amount for this Bill?");
+        var BillDate = prompt("What is the date that this Bill is due?");
+        WriteTo(BillName, BillPayment, BillDate);
+        var UserContinue = prompt("Do you have another Bill that you would like to enter? 1 for Yes or 0 for No");
+        if (UserContinue === 1) {
+            Continue = 1;
+        } else {
+            Continue = 0;
+        }
+    } while (Continue === 1);
+}
+
+
 
