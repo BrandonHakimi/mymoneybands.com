b699d830bdee01ccced264ab9d6f47702eb3fe1b
Chris-Araklisianos <christopher.araklisianos.733@my.csun.edu>
2022-11-07 19:06:58 -0800

    Added JS implementation of AddProjectedIncome and AddBudget
    Added Javascript functions for initial user set-up, adding the AddProjectedIncome method to input their projected income, the AddBudget method to input their budget, and revised the AddBills method to alert the user to the contents of a bill after they have inputted the data. Buttons in dashboard.html have also been added to call this functions on user click.

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,16 @@
 
+function AddProjectedIncome() {
+    var ProjectedIncome  = prompt("What is your Projected Income for this month?");
+    WriteTo(ProjectedIncome);
+    alert("Your projected income is $" + ProjectedIncome + ".");
+}
+
+function AddBudget() {
+    var Budget  = prompt("What is the amount of money that you wish to spend this month?");
+    WriteTo(Budget);
+    alert("Your budget is $" + Budget + ".");
+}
+
 function AddBill() {
     do {
         var Continue = 1;
@@ -6,6 +18,7 @@ function AddBill() {
         var BillValue = prompt("What is the Payment Amount for this Bill?");
         var BillDate = prompt("What is the date that this Bill is due?");
         WriteTo(BillName, BillPayment, BillDate);
+        alert("You have input a Bill due to " + BillName + "that will charge you " + BillPayment + " due on " + BillDate + ". ");
         var UserContinue = prompt("Do you have another Bill that you would like to enter? 1 for Yes or 0 for No");
         if (UserContinue === 1) {
             Continue = 1;
@@ -14,6 +27,8 @@ function AddBill() {
         }
     } while (Continue === 1);
 }
+
+
 
 
 
