41eddd24154732d24f96a7817c709f315c8e4ce4
hii314 <christian.foster88@gmail.com>
2022-11-15 11:40:12 -0800

    deleted a bunch of unnecessary files and started adding basic code for graph generation. I will look at and develop that code further in the future. For now, leave the code commented as for some reason no functions are identifiable when the code exists

--- JS/calculations.js
+++
@@ -1,8 +0,0 @@
-function TestMonthlySum(){
-    const Categories = [350, 50, 200, 150, 500, 400, 100, 25]
-    let Sum = 0;
-    for(let i = 0; i < Categories.length; i++){
-        Sum = Sum + Categories[i];
-    }
-    print(Sum);
-}
