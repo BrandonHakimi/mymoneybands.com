39de3ce64a2a7f091eff1e21be318566cf184275
Brandon Hakimi <hakimibrandon00@gmail.com>
2022-12-06 10:16:05 -0800

    Added signup-process.php and database.php and removed form.js and home.js

---
+++ PHP/signup-process.php
@@ -0,0 +1,34 @@
+<?php
+
+if (empty($_POST["fname"])) {
+    die("Name is required");
+}
+
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required");
+}
+
+if (strlen($_POST["password"]) < 8) {
+    die("Password must be at least 8 characters");
+}
+
+if (!preg_match("/[a-z]/i", $_POST["password"]) {
+    die("Password must contain at least one letter");
+}
+
+if (!preg_match("/[0-9]/i", $_POST["password"]) {
+    die("Password must contain at least one letter");
+}
+
+if ($_POST["password"] !== $_POST["cpassword"]) {
+    die("Passwords must match");
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/PHP/database.php";
+
+$sql = "INSERT INTO user (fname, email, password_hash"
+
+print_r($_POST); 
+var_dump($password_hash);
